7673efde45140afe9c34eb53378ca64aa23b4eaa
chenvle <chenvle@cmtime.cn>
2022-10-18 09:04:37 +0800

    优化代码

--- src/Excel.php
+++ src/Excel.php
@@ -11,98 +11,93 @@ class Excel
 {
     public function output($data, $title, $field, $fileType = 'Xlsx')
     {
-        try {
-            $en       = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-            $end_en   = $en[count($field) - 1];
-            $end_line = count($data) + 2;
+        $en       = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        $end_en   = $en[count($field) - 1];
+        $end_line = count($data) + 2;
 
-            $spreadsheet = new Spreadsheet();
+        $spreadsheet = new Spreadsheet();
 
-            //获取当前表
-            $sheet = $spreadsheet->getActiveSheet();
+        //获取当前表
+        $sheet = $spreadsheet->getActiveSheet();
 
-            //表名称
-            $sheet->setTitle($title);
+        //表名称
+        $sheet->setTitle($title);
 
-            //表标题
-            $sheet->setCellValue('A1', $title);
-            $sheet->getRowDimension('1')->setRowHeight(30);
+        //表标题
+        $sheet->setCellValue('A1', $title);
+        $sheet->getRowDimension('1')->setRowHeight(30);
 
 
-            //合并
-            $sheet->mergeCells('A1:' . $end_en . '1');
-            $styleArrayBody = [
-                //字体
-                'font' => [
-                    //加粗
-                    'bold'  => true,
+        //合并
+        $sheet->mergeCells('A1:' . $end_en . '1');
+        $styleArrayBody = [
+            //字体
+            'font' => [
+                //加粗
+                'bold'  => true,
+                //颜色
+                'color' => ['rgb' => '000000'],
+                //字体大小
+                'size'  => 16,
+                //字体名称
+                'name'  => 'Verdana'
+            ]
+        ];
+        $sheet->getStyle('A1:' . $end_en . '1')->applyFromArray($styleArrayBody);//A1:J2 设置样色
+
+
+        /*整体样式*/
+        $styleArrayBody = [
+            //边框
+            'borders'   => [
+                'allBorders' => [
+                    //边框
+                    'borderStyle' => Border::BORDER_THIN,
                     //颜色
-                    'color' => ['rgb' => '000000'],
-                    //字体大小
-                    'size'  => 16,
-                    //字体名称
-                    'name'  => 'Verdana'
-                ]
-            ];
-            $sheet->getStyle('A1:' . $end_en . '1')->applyFromArray($styleArrayBody);//A1:J2 设置样色
-
-
-            /*整体样式*/
-            $styleArrayBody = [
-                //边框
-                'borders'   => [
-                    'allBorders' => [
-                        //边框
-                        'borderStyle' => Border::BORDER_THIN,
-                        //颜色
-                        'color'       => ['argb' => '666666'],
-                    ],
+                    'color'       => ['argb' => '666666'],
                 ],
-                //居中
-                'alignment' => [
-                    //水平居中
-                    'horizontal' => Alignment::HORIZONTAL_CENTER,
-                    //垂直居中
-                    'vertical'   => Alignment::VERTICAL_CENTER
-                ],
-            ];
-            $sheet->getStyle('A1:' . $end_en . $end_line)->applyFromArray($styleArrayBody);//A1:J2 设置样色
+            ],
+            //居中
+            'alignment' => [
+                //水平居中
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+                //垂直居中
+                'vertical'   => Alignment::VERTICAL_CENTER
+            ],
+        ];
+        $sheet->getStyle('A1:' . $end_en . $end_line)->applyFromArray($styleArrayBody);//A1:J2 设置样色
 
-            //表头
-            $num = 0;
-            foreach ($field as $i => $o) {
-                //设置表头
-                $sheet->setCellValue($en[$num] . '2', $o['value']);
-                //列宽
-                $sheet->getColumnDimension($en[$num])->setWidth($o['width']);
-                $num++;
-            }
-
-            //表体
-            $num = 0;
-            foreach ($field as $i => $o) {
-                foreach ($data as $k => $datum) {
-                    $value = '';
-                    if (isset($o['with']) && $o['with'] && isset($datum[$o['with']][$i])) {
-                        $value = $datum[$o['with']][$i];
-                    } else if (isset($datum[$i])) {
-                        $value = $datum[$i];
-                    }
-                    if (isset($o['type']) && $o['type'] == 'string') {
-                        $value = $value . ' ';
-                    }
-                    $sheet->setCellValue($en[$num] . ($k + 3), $value);
-                }
-                $num++;
-            }
-
-            $writer = IOFactory::createWriter($spreadsheet, $fileType);
-            $this->excelBrowserExport(time(), $fileType);
-            $writer->save('php://output');
-
-        } catch (\Exception $e) {
-            dd($e->getMessage());
+        //表头
+        $num = 0;
+        foreach ($field as $i => $o) {
+            //设置表头
+            $sheet->setCellValue($en[$num] . '2', $o['value']);
+            //列宽
+            $sheet->getColumnDimension($en[$num])->setWidth($o['width']);
+            $num++;
         }
+
+        //表体
+        $num = 0;
+        foreach ($field as $i => $o) {
+            foreach ($data as $k => $datum) {
+                $value = '';
+                if (isset($o['with']) && $o['with'] && isset($datum[$o['with']][$i])) {
+                    $value = $datum[$o['with']][$i];
+                } else if (isset($datum[$i])) {
+                    $value = $datum[$i];
+                }
+                if (isset($o['type']) && $o['type'] == 'string') {
+                    $value = $value . ' ';
+                }
+                $sheet->setCellValue($en[$num] . ($k + 3), $value);
+            }
+            $num++;
+        }
+
+        $writer = IOFactory::createWriter($spreadsheet, $fileType);
+        $this->excelBrowserExport(time(), $fileType);
+        $writer->save('php://output');
     }
 
 
@@ -114,7 +109,6 @@ class Excel
      * @return array    //返回数据内容 [['A' => 'content']];
      * @throws \PhpOffice\PhpSpreadsheet\Calculation\Exception
      * @throws \PhpOffice\PhpSpreadsheet\Exception
-     * @throws \think\Exception
      */
     public function input($excelPath, $param, $startRow)
     {
